a5cf35b9b6fa410db2ffb359410e5e6edf840908
tory green <tory.green@gmail.com>
2022-07-04 17:27:25 +0400

    first

--- secondproject/tryme.py
+++ secondproject/tryme.py
@@ -1,5 +1,5 @@
 def try_me():
-    print("I am a dog.  Bark. Bark.  I am a cat.  Meow. Meow.")
+    print("I am a dog.  Bark. Bark.  I am a cat.  Meow. Meow. Meow.")
 
 if __name__ == "__main__":
     try_me()
